2f8d7fc6a3323d8ed3e11cfec933f846fac477b8
Nurzhan Bazhikov <nurzhan.bazhikov@gmail.com>
2025-06-01 17:09:39 +0200

    bastion

---
+++ base/bastion.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.subnet_cloudx[0].id
+
+  iam_instance_profile = aws_iam_instance_profile.ghost_app_profile.name
+
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+
+  tags = {
+    Name = "bastion-host"
+  }
+  
+}
